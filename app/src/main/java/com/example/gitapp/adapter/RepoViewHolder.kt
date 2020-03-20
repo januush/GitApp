2d8a67bdae5110c700ea98339a048427eec51d3b
januush <42897654+januush@users.noreply.github.com>
2020-03-20 17:00:17 +0100

    RepoViewHolder created

---
+++ app/src/main/java/com/example/gitapp/adapter/RepoViewHolder.kt
@@ -0,0 +1,32 @@
+package com.example.gitapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gitapp.R
+import com.example.gitapp.model.Repository
+import com.example.gitapp.util.getDrawable
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.repo_row.view.*
+
+class RepoViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+    private val progressDrawable = getDrawable(view.context)
+    fun bind(repo: Repository?){
+        itemView.repoName.text = repo?.repoName
+        itemView.githublink.text = repo?.repoLink
+
+        Picasso.get()
+            .load(repo?.repoImage?.avatar_url)
+            .placeholder(progressDrawable)
+            .into(itemView.repoImage)
+    }
+
+    companion object{
+        fun create(parent: ViewGroup): RepoViewHolder {
+            val layoutInflater= LayoutInflater.from(parent.context)
+            val view = layoutInflater.inflate(R.layout.repo_row,parent,false)
+            return RepoViewHolder(view)
+        }
+    }
+}
--- app/src/main/java/com/example/gitapp/adapter/ReposAdapter.kt
+++ app/src/main/java/com/example/gitapp/adapter/ReposAdapter.kt
@@ -4,24 +4,42 @@ import android.view.ViewGroup
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.gitapp.R
 import com.example.gitapp.model.Repository
+import com.example.gitapp.util.NetworkState
+import java.lang.IllegalArgumentException
 
 class ReposAdapter(private val retryCallback: () -> Unit) : PagedListAdapter<Repository, RecyclerView.ViewHolder>(RepoDiffCallback) {
 
+    private var networkState: NetworkState? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        return when (viewType) {
+            R.layout.repo_row -> RepoViewHolder.create(parent)
+            else -> throw IllegalArgumentException("unknown view type")
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        when (getItemViewType(position)){
+            R.layout.repo_row -> (holder as RepoViewHolder).bind(getItem(position))
+        }
+    }
+
+    private fun hasExtraRow(): Boolean {
+        return networkState != null && networkState != NetworkState.LOADED
     }
 
     override fun getItemViewType(position: Int): Int {
-        return super.getItemViewType(position)
+        return if(hasExtraRow() && position == itemCount - 1){
+            R.layout.item_network_state
+        } else{
+            R.layout.repo_row
+        }
     }
 
     override fun getItemCount(): Int {
-        return super.getItemCount()
+        return super.getItemCount() + if (hasExtraRow()) 1 else 0
     }
 
 
