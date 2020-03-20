4b5a67c6ca01912be4852c8a462344d73864f476
januush <42897654+januush@users.noreply.github.com>
2020-03-20 15:16:41 +0100

    Rebuilt API interface to handle the pagination

--- app/src/main/java/com/example/gitapp/model/ReposAPI.kt
+++
@@ -1,9 +0,0 @@
-package com.example.gitapp.model
-
-import io.reactivex.Single
-import retrofit2.http.GET
-
-interface ReposAPI {
-    @GET("/repositories")
-    fun getRepos(): Single<List<Repository>>
-}
--- app/src/main/java/com/example/gitapp/model/ReposService.kt
+++ app/src/main/java/com/example/gitapp/model/ReposService.kt
@@ -4,23 +4,23 @@ import io.reactivex.Single
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
-class ReposService {
+interface ReposService {
 
-    private val BASE_URL = "https://api.github.com"
-    private val api:ReposAPI
+    @GET("/repositories")
+    fun getRepos(@Query("since") id: Long): Single<List<Repository>>  //"since" (page), "per_page" unavailable for repos
 
-    init {
-        api = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .build()
-            .create(ReposAPI::class.java)
+    companion object {
+        fun getService(): ReposService {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+            return retrofit.create(ReposService::class.java)
+        }
     }
 
-    //The only function for getting the information from backend
-    fun getRepos(): Single<List<Repository>> {
-        return api.getRepos()
-    }
 }
